724654af8f1deba97873d6669fdddc7bf73c8cf4
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-04-19 19:13:27 +0530

    Basic Scrapy Spider

---
+++ choclate_scraper/choclate_scraper/spiders/choclate_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+class ChoclateSpiderSpider(scrapy.Spider):
+    # Title of spider
+    name = "choclate_spider"
+    allowed_domains = ["chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"]
+
+    def parse(self, response):
+        # we loop through products and extract name,price,url
+        products = response.css('product-item')
+        for product in products:
+            """Put the data returned into required format we want to output for csv or json file"""
+            yield{
+                'name':product.css('a.product-item-meta__title::text').get(),
+                'price':product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
+                'url':product.css('div.product-item-meta a').attrib['href'],
+            }
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+        if next_page is not None:
+            next_page_url = 'https://www.chocolate.co.uk'+next_page
+            yield response.follow(next_page_url,callback = self.parse)
